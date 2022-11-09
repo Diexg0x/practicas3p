10a86c2d12c77243b413f7b361659ee8400656a6
arturo <arturo@gmail.com>
2022-11-09 10:09:18 -0600

    diego es tonto

---
+++ 01_crud/main.js
@@ -0,0 +1,72 @@
+const cargarPosts=async()=>{
+    let url="https://jsonplaceholder.typicode.com/posts";
+    const api=await fetch(url);
+    const data=await api.json();
+    console.log(data);
+    tabla=document.querySelector("#lista");
+    data.map(item=>{ 
+    tabla.innerHTML+=`
+    <tr>
+                    <td scope="row">${item.id}</td>
+                    <td>${item.title}</td>
+                    <td>${item.body}</td>
+                    <td><button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#Sexoo"><i class="bi bi-brush"></i></i>Editar</button></td>
+                    <td><button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#Sexoo2"><i class="bi bi-x-octagon"></i>Eliminar</button></td>
+        </tr>
+    `})
+    }
+    
+    
+    const guardardata=async ()=>{
+    let titulo=document.querySelector("#title").value;
+    let body=document.querySelector("#body").value;
+    const post={title:titulo,body:body,userId:1};
+    
+    
+        const api= await  fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify (post),
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            },
+            });
+    const respuesta=await api.json();
+    console.log(respuesta)
+    tabla=document.querySelector("#lista");
+    tr=document.createElement("tr");
+    
+    tr.innerHTML=`
+    <tr>
+                    <td scope="row">${respuesta.id}</td>
+                    <td>${respuesta.title}</td>
+                    <td>${respuesta.body}</td>
+                    <td><button type="button" class="btn btn-outline-info"  data-bs-toggle="modal" data-bs-target="#Sexoo"><i class="bi bi-brush"></i></i>Editar</button></td>
+                    <td><button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#Sexoo2"><i class="bi bi-x-octagon"></i>Eliminar</button></td>
+        </tr>
+    `
+    tabla.appendChild(tr);
+    
+    if (respuesta!=null){
+        Swal.fire({
+            icon: 'success',
+            title: 'Insertar',
+            text: 'Se inserto correctamente',
+    
+          })
+    }
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'no se inserto algo fallo',
+    
+          })
+    }
+        
+    
+    }
+    const eliminarpost=async ()=>{
+        
+    tr.innerHTML="";
+    
+    }
